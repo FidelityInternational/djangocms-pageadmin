a38658dd6b38038db5caca053dffcfdd2e39e787
Noel da Costa <noeldacosta@gmail.com>
2019-03-11 14:03:21 +0000

    Moved all actions into a burger menu

--- djangocms_pageadmin/static/djangocms_pageadmin/js/actions.js
+++ djangocms_pageadmin/static/djangocms_pageadmin/js/actions.js
@@ -4,6 +4,97 @@
     }
 
     $(function() {
+
+        var createBurgerMenu = function(row) {
+
+            // create burger container
+            var container = document.createElement('DIV');
+            var cssclass = document.createAttribute('class');
+            cssclass.value = '';
+            container.setAttributeNode(cssclass);
+
+            // create anchor icon
+            var anchor = document.createElement('A');
+            var cssclass = document.createAttribute('class');
+            cssclass.value = 'btn cms-icon-menu cms-page-admin-action-btn ' +
+                'cms-btn cms-btn-default ' +
+                'cms-btn-no-border cms-icon cms-icon-menu closed';
+            anchor.setAttributeNode(cssclass);
+            var title = document.createAttribute('title');
+            title.value = 'Actions';
+            anchor.setAttributeNode(title);
+            container.appendChild(anchor);
+            
+            // create options container
+            var optionsContainer = document.createElement('DIV');
+            cssclass = document.createAttribute('class');
+
+            cssclass.value = 
+                // main selector for the menu
+                'cms-pagetree-dropdown-menu ' + 
+                // keeps the menu arrow in position
+                'cms-pagetree-dropdown-menu-arrow-right-top';
+            optionsContainer.setAttributeNode(cssclass);
+            var ul = document.createElement('UL');
+            cssclass = document.createAttribute('class');
+            cssclass.value = 'cms-pagetree-dropdown-menu-inner';
+            ul.setAttributeNode(cssclass);
+
+            // get the existing actions and move them into the options container
+            var li;
+            var text;
+            let actions = $(row).children('.field-list_actions');
+            if (!actions.length) {
+                // skip any rows without actions to avoid errors
+                return;
+            }
+            $(actions[0]).children('.cms-page-admin-action-btn').each( function (index, item) {
+                li = document.createElement('LI');
+                text = document.createTextNode(item.title);
+                li.appendChild(item);
+                item.appendChild(text);
+                ul.appendChild(li);
+            })
+            optionsContainer.appendChild(ul);
+            actions[0].appendChild(container);
+            document.body.appendChild(optionsContainer);
+
+            anchor.addEventListener('click', function (ev){
+                toggleBurgerMenu(anchor, optionsContainer);
+            })
+        }
+
+
+        var toggleBurgerMenu = function(burgerMenuAnchor, optionsContainer) {
+            var bm = $(burgerMenuAnchor);
+            var op = $(optionsContainer);
+            var closed = bm.hasClass('closed');
+            $('.cms-pagetree-dropdown-menu').removeClass('open');
+            $('.cms-pagetree-dropdown-menu').addClass('closed');
+            $('.cms-btn-no-border').removeClass('open');
+            $('.cms-btn-no-border').addClass('closed');
+            if (closed) {
+                bm.removeClass('closed');
+                bm.addClass('open');
+                op.removeClass('closed');
+                op.addClass('open');
+            } else {
+                bm.addClass('closed');
+                bm.removeClass('open');
+                op.addClass('closed');
+                op.removeClass('open');
+            }
+            var pos = bm.offset();
+            op.css('left', pos.left - 200);
+            op.css('top', pos.top);
+        }
+
+        
+        $('#result_list').find('tr').each( function (index, item) {
+            createBurgerMenu(item);
+        })
+
+
         // it is not possible to put a form inside a form, so
         // actions have to create their own form on click
         $('.js-page-admin-action, .cms-page-admin-js-publish-btn, .cms-page-admin-js-edit-btn')
