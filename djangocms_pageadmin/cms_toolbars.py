e619e5903f282882391b1509cfdf03d98ba661aa
Noel da Costa <noeldacosta@gmail.com>
2019-03-27 11:36:43 +0000

    Added check for url presence
    - also ran black

--- djangocms_pageadmin/cms_toolbars.py
+++ djangocms_pageadmin/cms_toolbars.py
@@ -8,22 +8,18 @@ from cms.cms_toolbars import PageToolbar
 from cms.toolbar.items import ButtonList
 from cms.toolbar_pool import toolbar_pool
 from cms.utils import get_language_from_request, page_permissions
-from cms.utils.page_permissions import (
-    user_can_change_page,
-    user_can_delete_page,
-)
+from cms.utils.page_permissions import user_can_change_page, user_can_delete_page
 from cms.utils.urlutils import add_url_parameters, admin_reverse
 
 from djangocms_versioning.models import Version
 
 
 # Identifiers for search
-ADMIN_MENU_IDENTIFIER = 'admin-menu'
-VERSIONING_MENU_IDENTIFIER = 'version'
+ADMIN_MENU_IDENTIFIER = "admin-menu"
+VERSIONING_MENU_IDENTIFIER = "version"
 
 
 class PageAdminToolbar(PageToolbar):
-
     def change_admin_menu(self):
         """
         The standard Pages menu for the PageContent model as provided by the CMS returns a selected item.
@@ -33,21 +29,24 @@ class PageAdminToolbar(PageToolbar):
         super().change_admin_menu()
         menu = self.toolbar.get_menu(ADMIN_MENU_IDENTIFIER)
         item = menu.items[0]
-        if item and "admin/cms/pagecontent/" in item.url:
-            url = admin_reverse('cms_pagecontent_changelist')  # cms page admin
+        if item and item.url and "admin/cms/pagecontent/" in item.url:
+            url = admin_reverse("cms_pagecontent_changelist")  # cms page admin
             item.url = url
 
 
 def replace_toolbar(old, new):
-    """Replace `old` toolbar class with `new` class,
+    """
+    Replace `old` toolbar class with `new` class,
     while keeping its position in toolbar_pool.
     """
-    new_name = '.'.join((new.__module__, new.__name__))
-    old_name = '.'.join((old.__module__, old.__name__))
-    toolbar_pool.toolbars = OrderedDict([
-        (new_name, new) if name == old_name else (name, toolbar)
-        for name, toolbar in toolbar_pool.toolbars.items()
-    ])
+    new_name = ".".join((new.__module__, new.__name__))
+    old_name = ".".join((old.__module__, old.__name__))
+    toolbar_pool.toolbars = OrderedDict(
+        [
+            (new_name, new) if name == old_name else (name, toolbar)
+            for name, toolbar in toolbar_pool.toolbars.items()
+        ]
+    )
 
 
 replace_toolbar(PageToolbar, PageAdminToolbar)
