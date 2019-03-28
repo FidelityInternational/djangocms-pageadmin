cefa7e1abf1ee5d08d52c8716ea7a74ab5ce4863
Noel da Costa <noeldacosta@gmail.com>
2019-03-28 13:13:30 +0000

    Removed unnecessary reference to Versioning

--- djangocms_pageadmin/test_utils/helpers.py
+++ djangocms_pageadmin/test_utils/helpers.py
@@ -2,8 +2,6 @@ from django.test import RequestFactory
 
 from cms.toolbar.toolbar import CMSToolbar
 
-from djangocms_versioning.cms_toolbars import VersioningToolbar
-
 from djangocms_pageadmin.test_utils.factories import UserFactory
 
 
@@ -24,8 +22,6 @@ def get_toolbar(content_obj, user=None, **kwargs):
     # Set the toolbar class
     if kwargs.get("toolbar_class", False):
         toolbar_class = kwargs.get("toolbar_class")
-    else:
-        toolbar_class = VersioningToolbar
     toolbar = toolbar_class(
         request, toolbar=request.toolbar, is_current_app=True, app_path="/"
     )
