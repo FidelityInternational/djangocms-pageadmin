f3d72eed3acc9e1fa1c29c3498eaf70021fd35bb
Stuart Axon <stuaxo2@yahoo.com>
2020-03-30 12:49:00 +0100

    Update cms_toolbars.py
    Extra commit to force rebuild.

--- djangocms_pageadmin/cms_toolbars.py
+++ djangocms_pageadmin/cms_toolbars.py
@@ -20,7 +20,7 @@ class PageAdminToolbar(PageToolbar):
         super().change_admin_menu()
         menu = self.toolbar.get_menu(ADMIN_MENU_IDENTIFIER)
         item = menu.items[0]
-        # Handle menu items like SubMenu that have no url attribute:
+        # Menu items like SubMenu that have no url attribute:
         if item and getattr(item, 'url', None) and "admin/cms/pagecontent/" in item.url:
             url = admin_reverse("cms_pagecontent_changelist")  # cms page admin
             item.url = url
