1498a5842a6c57fcab9e4f40355339c2d128c182
Andrew Aikman <andrew.aikman@eliga.services>
2021-09-24 15:55:40 +0100

    Remove incorrect package mappings

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ INSTALL_REQUIREMENTS = [
     "Django>=1.11,<3.0",
     "django_cms",
     "django-treebeard>=4.3",
-    "djangocms_versioning",
-    "djangocms_version_locking",
 ]
 
 TESTS_REQUIRE = [
@@ -39,9 +37,4 @@ setup(
     license="BSD",
     test_suite="tests.settings.run",
     tests_require=TESTS_REQUIRE,
-    dependency_links=[
-        "http://github.com/divio/django-cms/tarball/release/4.0.x#egg=django-cms-4.0.0",
-        "http://github.com/divio/djangocms-versioning/tarball/master#egg=djangocms-versioning-0.0.23",
-        "http://github.com/FidelityInternational/djangocms-version-locking/tarball/master#egg=djangocms-version-locking-0.0.13"  # noqa
-    ]
 )
