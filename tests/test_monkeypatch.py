958f03504362b5d1ef80753b49d5708416495f94
Adam Murray <adam-murray@hotmail.co.uk>
2022-06-24 18:12:10 +0100

    fix: Added tests to reflect changes to monkeypatch target

--- tests/test_monkeypatch.py
+++ tests/test_monkeypatch.py
@@ -1,5 +1,8 @@
 from cms.test_utils.testcases import CMSTestCase
-from cms.toolbar.utils import get_object_edit_url, get_object_preview_url
+from cms.toolbar.utils import get_object_preview_url, get_object_edit_url
+
+from djangocms_versioning.constants import PUBLISHED
+from djangocms_versioning.test_utils.test_helpers import find_toolbar_buttons, get_toolbar
 
 from djangocms_pageadmin.test_utils import factories
 
@@ -7,15 +10,16 @@ from djangocms_pageadmin.test_utils import factories
 class ToolbarMonkeyPatchTestCase(CMSTestCase):
     def test_preview_button_contains_target(self):
         """
-        The monkeypatch adds the target attribute to the preview button only
+        The monkeypatch adds the target attribute to the view published button
         """
-        user = self.get_superuser()
-        version = factories.PageVersionFactory(created_by=user, content__template="page.html")
-        version.publish(user)
-        url = get_object_edit_url(version.content)
+        version = factories.PageVersionFactory(content__template="page.html", state=PUBLISHED)
+        toolbar = get_toolbar(version.content, preview_mode=True)
+        toolbar.post_template_populate()
 
-        with self.login_user_context(user):
-            response = self.client.get(url)
+        button = toolbar.toolbar.get_right_items()[1].buttons[0]
+        rendered_button = button.render()
 
-        self.assertContains(response, 'class="cms-btn cms-btn cms-btn-switch-save" target="_blank"')
-        self.assertContains(response, get_object_preview_url(version.content))
+        self.assertIn(
+            'class="cms-btn cms-btn cms-btn-switch-save" target="_blank" >View Published</a>', rendered_button
+        )
+        self.assertIn(version.content.get_absolute_url(), rendered_button)
