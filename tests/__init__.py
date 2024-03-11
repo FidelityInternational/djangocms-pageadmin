ce29b100187fe1e1433b009d6b3748d8acbf30f6
傅瑞曼 <yxzfreeman@gmail.com>
2024-03-11 06:47:57 +0000

    code enhance

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
-import django
+from djangocms_pageadmin.compat import DJANGO_4_2
 
 
-if django.VERSION < (4, 2):  # TODO: remove when dropping support for Django < 4.2
+if not DJANGO_4_2:  # TODO: remove when dropping support for Django < 4.2
     from django.test.testcases import TransactionTestCase
 
     TransactionTestCase.assertQuerySetEqual = TransactionTestCase.assertQuerysetEqual
